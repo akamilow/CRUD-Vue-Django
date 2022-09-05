e26d23ff89d698bf3bae4b1fcd6b95d94dc7b45e
Camilo Castellar <camilocastelar3@gmail.com>
2022-09-05 15:34:53 -0500

    urls

--- InventarioApp/urls.py
+++ InventarioApp/urls.py
@@ -4,4 +4,10 @@ from InventarioApp import views
 urlpatterns = [
     url(r'^producto$', views.producto_list),
     url(r'^producto/([0-9]+)$', views.producto_list),
+    url(r'^cocina$', views.cocina_list),
+    url(r'^cocina/([0-9]+)$', views.cocina_list),
+    url(r'^limpieza$', views.limpieza_list),
+    url(r'^limpieza/([0-9]+)$', views.limpieza_list),
+    url(r'^proveedor$', views.proveedor_list),
+    url(r'^proveedor/([0-9]+)$', views.proveedor_list)
 ]
