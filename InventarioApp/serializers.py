81dbfc5da12a272df35a4a6f03a25e6bb18806f4
Camilo Castellar <camilocastelar3@gmail.com>
2022-09-05 15:34:30 -0500

    serializers

--- InventarioApp/serializers.py
+++ InventarioApp/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from InventarioApp.models import Producto
+from InventarioApp.models import Cocina
+from InventarioApp.models import Limpieza
+from InventarioApp.models import Proveedor
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +13,40 @@ class ProductoSerializer(serializers.ModelSerializer):
             'categoria': {'required': True},
             'precio': {'required': True},
             'cantidad': {'required': True},
+        }
+
+class CocinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cocina
+        fields = ('id_pro_cocina', 'nombre', 'cantidad', 'precio', 'proveedor')
+        extra_kwargs = {
+            'nombre': {'required': True},
+            'cantidad': {'required': True},
+            'precio': {'required': True},
+            'proveedor': {'required': True},
+        }
+
+class LimpiezaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Limpieza
+        fields = ('id_pro_limpieza', 'nombre', 'cantidad', 'precio', 'proveedor')
+        extra_kwargs = {
+            'nombre': {'required': True},
+            'cantidad': {'required': True},
+            'precio': {'required': True},
+            'proveedor': {'required': True},
+        }
+
+class ProveedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proveedor
+        fields = ('id_proveedor', 'nombre', 'telefono', 'direccion', 'correo', 'producto', 'fecha', 'precio_total')
+        extra_kwargs = {
+            'nombre': {'required': True},
+            'telefono': {'required': True},
+            'direccion': {'required': True},
+            'correo': {'required': True},
+            'producto': {'required': True},
+            'fecha': {'required': True},
+            'precio_total': {'required': True},
         }
